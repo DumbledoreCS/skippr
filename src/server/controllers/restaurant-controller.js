92c0ed4ca898ed68e24c2e68db6857cabad11a37
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-12-05 15:10:44 -0800

    added axios as dependency, input address will also store coordinated via google api

--- src/server/controllers/restaurant-controller.js
+++ src/server/controllers/restaurant-controller.js
@@ -1,19 +1,39 @@
 const pgClient = require('../models/database');
+const axios = require('axios');
+
 
 // STRETCH FEATURE: write new restaurant row to restaurants table
 function createRest(req, res) {
+  // deconstructs properties stored in request body
   const { name, email, password, address, city, state, zipCode, phone, yelpLink, imageLink } = req.body;
-  const values = [name, email, password, address, city, state, zipCode, phone, yelpLink, imageLink];
-  const createRestStr = 'INSERT INTO restaurants (rest_name, rest_email, rest_password, rest_address, rest_city, rest_state, rest_zipcode, rest_phone, rest_yelp_link, rest_imagelink) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;';
-  pgClient.query(createRestStr, values, (err, result) => {
-    if (err) res.status(400).json({ error: 'Unable to create a restaurant account' });
-    else {
-      res.status(200).json({
-        message: 'New restaurant account has been successfully created',
-        restaurant: result.rows[0],
+  // create correctly formatted address for request
+  let formatAddress = `${address}, ${city}, ${state}, ${zipCode}`;
+  // send request to google api
+  // when request returns, store longitude and latitude into respective labels
+  // include long and lat as input to store into via values array
+  // insert data into database
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+    params: {
+      address: formatAddress,
+      key: 'AIzaSyAIDCvHRxSebeeGzY38HO1fFBCUY111onc',
+    },
+  })
+    .then((response) => {
+      const lat = response.data.results[0].geometry.location.lat;
+      const lng = response.data.results[0].geometry.location.lng;
+      const values = [name, email, password, lat, lng, address, city, state, zipCode, phone, yelpLink, imageLink];
+      const createRestStr = 'INSERT INTO restaurants (name, email, password, latitude, longitude, address, city, state, zipcode, phone, yelp_link, image_link) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;';
+      pgClient.query(createRestStr, values, (err, result) => {
+        if (err) res.status(400).json({ error: 'Unable to create a restaurant account' });
+        else {
+          res.status(200).json({
+            message: 'New restaurant account has been successfully create',
+            restaurant: result.rows[0],
+          });
+        }
       });
-    }
-  });
+    })
+    .catch(err => res.send(err));
 }
 
 // fetch restaurant email and password match from restaurants table
